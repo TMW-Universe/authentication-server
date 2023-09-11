b1396f9349506a0be96121a3a9ebea93bbe457e2
Joel Campos <joelcamposoliva9000@gmail.com>
2023-09-11 12:47:59 +0200

    Reorder

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from 'src/database/repositories/user.repository';
 import { WrongCredentialsException } from 'src/errors/auth/wrong-credentials.exception';
+import { getEnv } from 'src/utils/config/get-env';
 import { compareHashWithSalt } from 'src/utils/cryptography/cryptography';
 
 @Injectable()
@@ -19,18 +20,24 @@ export class AuthService {
     // If user is not found
     if (!user) throw new WrongCredentialsException();
 
-    if (user.getDataValue('twoFaToken')) {
-      // If 2FA is enabled
-
-      if (!credentials.twoFaCode) {
-        // If the user has not provided a 2FA code
-        return { requires2Fa: true };
-      }
-    }
-
     if (
       compareHashWithSalt(user.getDataValue('password'), credentials.password)
     ) {
+      // First check for 2FA
+
+      if (user.getDataValue('twoFaToken')) {
+        // If 2FA is enabled
+
+        if (!credentials.twoFaCode) {
+          // If the user has not provided a 2FA code
+          return { requires2Fa: true };
+        }
+      }
+
+      // Generate token
+
+      const { jwtSecret } = getEnv();
+
       return {
         accessToken: 'TOKEN',
       };
