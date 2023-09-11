5c6a683523533657d6c201d4bad6102aef3238eb
Joel Campos <joelcamposoliva9000@gmail.com>
2023-09-11 23:03:13 +0200

    Increase wait time on failed login

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -7,7 +7,7 @@ import { Throttle } from '@nestjs/throttler';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Throttle({ default: { limit: 8, ttl: 30000 } })
+  @Throttle({ default: { limit: 7, ttl: 60000 } })
   @HttpCode(HttpStatus.OK)
   @Post('login')
   async login(@Body() body: LoginDTO) {
