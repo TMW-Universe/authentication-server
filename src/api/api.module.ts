e70276973389412cad1f2041e84e15e511024a91
Joel Campos <joelcamposoliva9000@gmail.com>
2023-09-11 22:56:18 +0200

    Fix: throttler limit

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -4,10 +4,18 @@ import { ApiService } from './api.service';
 import { AuthModule } from './auth/auth.module';
 import { DomainsModule } from './domains/domains.module';
 import { ThirdPartyModule } from './third-party/third-party.module';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
   controllers: [ApiController],
-  providers: [ApiService],
+  providers: [
+    ApiService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   imports: [AuthModule, DomainsModule, ThirdPartyModule],
 })
 export class ApiModule {}
