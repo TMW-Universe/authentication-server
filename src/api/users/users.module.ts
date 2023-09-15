261f927eb378d404a063df566061bb3f4d49d60e
Joel Campos <joelcamposoliva9000@gmail.com>
2023-09-15 17:39:07 +0200

    Fix: JWT module

--- src/api/users/users.module.ts
+++ src/api/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UserRepository } from 'src/database/repositories/user.repository';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UserRepository],
 })
 export class UsersModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './guards/authentication/auth.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { getEnv } from './utils/config/get-env';
 
 @Module({
   imports: [
@@ -13,6 +15,12 @@ import { AuthGuard } from './guards/authentication/auth.guard';
         limit: 60,
       },
     ]),
+    JwtModule.register({
+      global: true,
+      publicKey: getEnv().jwtPublicKey,
+      privateKey: getEnv().jwtPrivateKey,
+      signOptions: { expiresIn: '45d' },
+    }),
     DatabaseModule,
     ApiModule,
   ],
