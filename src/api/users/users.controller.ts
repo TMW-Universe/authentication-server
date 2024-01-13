ca1622a86378ef6abb43734664dfa8fb7767a2ce
Joel Campos <joelcamposoliva9000@gmail.com>
2024-01-13 14:16:10 +0100

    Update by userId

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from 'src/decorators/user/user.decorator';
-import { UserProfile } from '@prisma/client';
+import { User as UserModel } from '@prisma/client';
 import { UpdateUserProfileName } from '../../dtos/users/update-user-profile.name.dto';
 
 @Controller('users')
@@ -9,15 +9,15 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('profile')
-  async getProfile(@User() user: UserProfile) {
+  async getProfile(@User() user: UserModel) {
     return await this.usersService.getUserProfile(user.id);
   }
 
   @Put('profile/name')
   async updateUserProfile(
-    @User() { userId }: UserProfile,
+    @User() { id }: UserModel,
     @Body() body: UpdateUserProfileName,
   ) {
-    return await this.usersService.updateUserProfileName(userId, body);
+    return await this.usersService.updateUserProfileName(id, body);
   }
 }
